8079bf266238460a3111908a560f6005ab1a7bc6
jun9771 <jun97718@gmail.com>
2024-10-17 17:33:58 +0900

    OOOcampaign

--- src/api/CampaignApi.jsx
+++ src/api/CampaignApi.jsx
@@ -5,6 +5,7 @@ const API_URL = 'http://localhost:8080/api/campaigns';
 export const fetchCampaignData = async () => {
     try {
         const response = await axios.get(API_URL);
+        console.log('API Response:', response.data);
         return response.data; // json형식으로
     } catch (error) {
         console.error('Error fetching campaign data:', error);
--- src/components/CampaginTable.jsx
+++ src/components/CampaginTable.jsx
@@ -8,24 +8,28 @@ const CampaignTable = () => {
     const { data, isLoading } = useCampaignData();
 
     const columns = React.useMemo(() => [
-        { Header: '캠페인 ID', accessor: 'campaignId' },
+        { Header: '캠페인 ID', accessor: 'campaign_id' },
         { Header: '분류1', accessor: 'category1' },
         { Header: '분류2', accessor: 'category2' },
-        { Header: '캠페인명', accessor: 'campaignName' },
+        { Header: '캠페인명', accessor: 'campaign_name' },
         { Header: '상태', accessor: 'status' },
-        { Header: '시작 일자', accessor: 'startDate' },
-        { Header: '종료 일자', accessor: 'endDate' },
-        { Header: '공개 여부', accessor: 'isPublic' },
+        { Header: '시작 일자', accessor: 'start_date' },
+        { Header: '종료 일자', accessor: 'end_date' },
+        { Header: '공개 여부', accessor: 'is_public' },
         { Header: '기안 부서', accessor: 'department' },
         { Header: '기안자', accessor: 'author' },
-        { Header: '기안일자', accessor: 'createdDate' },
-        { Header: '태그', accessor: 'tags', Cell: ({ value }) => value.join(', ') },
+        { Header: '기안일자', accessor: 'created_date' },
+       // { Header: '태그', accessor: 'tags', Cell: ({ value }) => value.join(', ') },
     ], []);
 
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, } = useTable({ columns, data });
 
     if (isLoading) {
         return <div>데이터를 불러오는 중...</div>;
+    }
+
+    if (data.length === 0) {
+        return <div>Loading...</div>;
     }
 
     return (
