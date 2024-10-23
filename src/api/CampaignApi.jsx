46c3715cddfd880a5f05eb44329c194dbadad712
xlzbthxyjkoo <happyyj01@naver.com>
2024-10-23 17:09:24 +0900

    fix

--- src/api/CampaignApi.jsx
+++ src/api/CampaignApi.jsx
@@ -43,7 +43,7 @@ export const createCampaignData = async (formData) => {
       tags: formData.tags,
     };
     console.log("Sending data to server:", campaignDTO);
-    console.log(api.post("/campaigns/add"));
+    // console.log(api.post("/campaigns/add"));
     const response = await api.post("/campaigns/add", campaignDTO);
     return response.data;
   } catch (error) {
