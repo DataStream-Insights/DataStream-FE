aa480c0992db0384f42a3300f862ebdb6d9e2df3
jun9771 <jun97718@gmail.com>
2024-10-29 15:05:52 +0900

    samplemainadd

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { CreateCampaign } from "./pages/campaign/createCampaign";
 import { CampaignManagementPage } from "./pages/main/CampaignManagementPage";
 import LogFormatPage from "./pages/format/format";
 import LogFilter from "./components/LogFilter";
+import SamplemainPage  from "./pages/main/samplemain";
 
 export function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <CampaignManagementPage /> },
+    { path: "/", element: <SamplemainPage /> },
+
+    { path: "/campaignmanagement", element: <CampaignManagementPage /> },
 
     { path: "/campaignform", element: <CreateCampaign /> }, //캠페인 생성하기
 
---
+++ src/pages/main/samplemain.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SamplemainPage =() =>{
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>메인 샘플</h1>
+
+            <div>
+                <button
+                    onClick={() => navigate('/campaignmanagement')}
+                    className="px-16 py-8 bg-blue-500 text-white rounded hover:bg-blue-600"
+                >campaignmanagement page</button>
+                <br />
+                <br />
+                <br />
+                <button
+                    onClick={() => navigate('/format')}
+                    className="px-16 py-8 bg-blue-500 text-white rounded hover:bg-blue-600"
+                >format page</button>
+                <br />
+                <br />
+                <br />
+                <button
+                    onClick={() => navigate('/filter')}
+                    className="px-16 py-8 bg-blue-500 text-white rounded hover:bg-blue-600"
+                >filter page</button>
+                
+            </div>
+        </div>
+    );
+}
+
+export default SamplemainPage;
