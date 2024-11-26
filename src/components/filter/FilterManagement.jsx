349c1fcdb57d073dbc1e89055d002fe32241dead
xlzbthxyjkoo <happyyj01@naver.com>
2024-11-26 14:38:19 +0900

    feat: update filter list, scroll

--- src/components/filter/FilterManagement.jsx
+++ src/components/filter/FilterManagement.jsx
@@ -1,6 +1,5 @@
 // FilterManagement.jsx
-import React from "react";
-
+import React, { useEffect } from "react";
 import { useTable, useRowSelect } from "react-table";
 import { Search, Plus, MoreVertical, X } from "lucide-react";
 import { useParams } from "react-router-dom";
@@ -10,17 +9,44 @@ import * as S from "../../styles/main/tableStyle";
 import Loading from "../../components/Loading";
 export function FilterManagement({ onSelect, onCreate, isDetailVisible }) {
   const { campaignId, formatId } = useParams();
-  const { data, isLoading } = useFilterData();
+  const { data, isLoading, loadFilters } = useFilterData();
 
   const handleRowClick = (row) => {
+    const duration = 800; // 애니메이션 지속 시간 (밀리초)
+    const start = window.pageYOffset;
+    const startTime =
+      "now" in window.performance ? performance.now() : new Date().getTime();
+
+    const scroll = () => {
+      const now =
+        "now" in window.performance ? performance.now() : new Date().getTime();
+      const time = Math.min(1, (now - startTime) / duration);
+
+      // easeInOutQuad 이징 함수 사용
+      const timeFunction =
+        time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
+
+      window.scrollTo(0, start * (1 - timeFunction));
+
+      if (time < 1) {
+        requestAnimationFrame(scroll);
+      }
+    };
+
+    requestAnimationFrame(scroll);
     onSelect(row.original);
   };
 
-  const handleCreateClick = async () => {
+  const handleCreateClick = () => {
     if (onCreate) {
-      await onCreate(); // 새로고침 처리
+      onCreate(); // 새로고침 처리
     }
   };
+
+  // data가 변경될 때마다 테이블 업데이트
+  useEffect(() => {
+    loadFilters();
+  }, [loadFilters, isDetailVisible]);
 
   const columns = React.useMemo(
     () => [
